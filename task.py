e988229669c5cc672a6dd26230a5ffb7fe2243e6
Nipun Khanna <2Nipun@gmail.com>
2021-01-23 23:47:29 -0800

    added news single support
    also renamed files a bit. Need to rename folder too to CMS later

--- task.py
+++ task.py
@@ -35,34 +35,34 @@ def uploadData(inputDictList, recToUpdate):
 
 
 # Gives back correct news data based on ask ie how many, and what format
-def cleanNewsData(inputNews, inputDataFormat, count_asked):
-	data_asked = inputDataFormat["data_needed"]
-	# print ('Data asked',data_asked)
+def getNewsData(inputNews, inputDataFormat):
+	type_asked = inputDataFormat["type"] #dataTable or dataSingle
+	data_asked = inputDataFormat["data_needed"] #format of data asked
 	
-	range_to_check = count_asked if (count_asked <= len(inputNews)) else len(inputNews)
-
-	if count_asked == 1: #In case only 1 item asked for or available 
+	#Different functions if List or Single data asked for
+	if type_asked == 'dataSingle': #In case only 1 item asked for or available 
 		tempDict = {} #since this will return a single dict
-		recIDneeded = inputDataFormat["recIDneeded"]
-		if recIDneeded > len(inputNews):
+		recID_asked = inputDataFormat["recID_needed"] #which rec needed, only used if dataSingle
+		if recID_asked > len(inputNews):
 			return {"error":"🚫 Record asked not in dict"}
 		else:
 			for i in inputNews:
-				if (str(i['recID_article']) == str(recIDneeded)): #Only returning that recID
+				if (str(i['recID']) == str(recID_asked)): #Only returning that recID
 					for key, value in data_asked.items(): #To map it
 						if value in i.keys(): 
 							tempDict[key] = i[value]
 			return tempDict
 	
-	elif count_asked > 1: #In case of newsTable
+	elif type_asked == 'dataTable': #In case of newsTable
+		count_asked = inputDataFormat["count_needed"] #how many needed, only used if dataTable
+		range_to_check = count_asked if (count_asked <= len(inputNews)) else len(inputNews)
 		outputDict = {
 				"table_info": {
-					"type":"newsSingleTable",
+					"type":"dataTable",
 				},
 				"rows" : []
 			}
 		outputList = []		
-
 		for rec in range(range_to_check): 
 			tempDict = {}
 			tempDict['recID'] = rec #to give it a sequence
@@ -76,24 +76,23 @@ def cleanNewsData(inputNews, inputDataFormat, count_asked):
 		return outputDict
 	
 	else:
-		return "🚫Data input incorrect"
+		return {"error":"🚫Data input incorrect"}
 
 #Goes through all records and updates ones that are in the master dict
 def updateLoop():
-	allRecords = airtable_producer.get_all(view='Service - amDataNews')
-	
+	allRecords_news = airtable_producer.get_all(view='Service - amDataNews')
+	allRecords_images = airtable_producer.get_all(view='Service - amImagePuller')
+	allRecords = allRecords_news + allRecords_images
 	for i in allRecords:
 		try: #In case have a prod payload or anything wrong 
 			if "Prod_Ready" in i["fields"]: #Only working on prod ready ie checkboxed
 				rec_ofAsked = i["id"]
 				payload_native = i["fields"]["payload"]
 				payload_json = json.loads(payload_native)
-				count_asked = payload_json["count_needed"] #How many records needed 
-				# data_asked = payload_json["data_needed"]
-				data_asked = payload_json #Giving it entire JSON like in amDatacovid
+				data_asked_json = payload_json #Giving it entire JSON like in amDatacovid
 				news_output = i["fields"]["output - amPayload_News"][0] #Since airtable stores as a list
 				news_output_json = ast.literal_eval(news_output) #since List from airtable is in String
-				data_toUpload = cleanNewsData(news_output_json, data_asked, count_asked)
+				data_toUpload = getNewsData(news_output_json, data_asked_json)
 				uploadData(data_toUpload, rec_ofAsked) #Just that bit updated 
 				print ("Row upload to CMS done..")
 		except Exception: 
